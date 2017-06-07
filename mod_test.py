6b2f82326ec8cc6b002d997e6e056af916e10d6c
chirimenmonster <chirimenmonster@users.noreply.github.com>
2017-06-07 22:43:25 +0900

    パネル有効な戦闘モードの限定、角度表示の修正、貫通判定コードのコピー

--- mod_test.py
+++ mod_test.py
@@ -2,10 +2,13 @@ import math
 import BigWorld
 import GUI
 from debug_utils import LOG_CURRENT_EXCEPTION
+import BattleReplay
+from constants import ARENA_GUI_TYPE
+from gui.battle_control import avatar_getter
 from gui.Scaleform.daapi.view.battle.shared.crosshair import plugins
 from gui.Scaleform.daapi.view.battle.shared.crosshair.plugins import ShotResultIndicatorPlugin
 from AvatarInputHandler import gun_marker_ctrl
-
+from AvatarInputHandler import aih_constants
     
 class IndicatorPanel(object):
 
@@ -73,16 +76,17 @@ class IndicatorPanel(object):
     def setVisible(self, visible):
         self.window.visible = visible
 
-    def setInfo(self, armor, angle, pierced):
-        result = ('UNDEFINED', 'NOT_PIERCED', 'LITTLE_PIERCED', 'GREAT_PIERCED')[pierced]
-        msg = 'armor={:.0f}, angle={:.1f}, {}'.format(armor, math.degrees(angle), pierced)
-        BigWorld.logInfo('test', 'modified gunmarker: {}'.format(msg), None)
-        self.labels['valueArmor'].text = '{:.0f}'.format(armor)
-        self.labels['valueAngle'].text = '{:.1f}'.format(math.degrees(angle))
-        self.labels['valuePierced'].text = result if pierced > 0 else ''
-
+    def setInfo(self, armor, hitAngleCos, pierced):
+        s_armor = '{:.1f} ({:.1f})'.format(armor, armor / hitAngleCos) if armor is not None else ''
+        s_angle = '{:.1f}'.format(math.degrees(math.acos(hitAngleCos))) if hitAngleCos is not None else ''
+        s_pierced = ('UNDEFINED', 'NOT_PIERCED', 'LITTLE_PIERCED', 'GREAT_PIERCED')[pierced] if pierced is not None else ''
+        BigWorld.logInfo('test', 'modified gunmarker: armor={}, angle={}, piercded={}'.format(s_armor, s_angle, s_pierced), None)
+        self.labels['valueArmor'].text = s_armor
+        self.labels['valueAngle'].text = s_angle
+        self.labels['valuePierced'].text = s_pierced
 
 class ShotResultIndicatorPluginModified(ShotResultIndicatorPlugin):
+    indicator = None
 
     def start(self):
         super(ShotResultIndicatorPluginModified, self).start()
@@ -90,8 +94,10 @@ class ShotResultIndicatorPluginModified(ShotResultIndicatorPlugin):
         if ctrl is not None:
             ctrl.onGunMarkerStateChanged -= self._ShotResultIndicatorPlugin__onGunMarkerStateChanged
             ctrl.onGunMarkerStateChanged += self.__onGunMarkerStateChanged
-        self.indicator = IndicatorPanel()
-        self.indicator.start()
+        print 'guiType = {} {}'.format(avatar_getter.getArena().guiType, ARENA_GUI_TYPE.TRAINING)
+        if BattleReplay.isPlaying() or avatar_getter.getArena().guiType == ARENA_GUI_TYPE.TRAINING:
+            self.indicator = IndicatorPanel()
+            self.indicator.start()
         return
     
     def stop(self):
@@ -99,17 +105,21 @@ class ShotResultIndicatorPluginModified(ShotResultIndicatorPlugin):
         ctrl = self.sessionProvider.shared.crosshair
         if ctrl is not None:
             ctrl.onGunMarkersSetChanged -= self.__onGunMarkerStateChanged
-        self.indicator.stop()
+        if self.indicator:
+            self.indicator.stop()
+            self.indicator = None
         return
 
     def __updateColor(self, markerType, position, collision):
         self._ShotResultIndicatorPlugin__updateColor(markerType, position, collision)
         result = gun_marker_ctrl.getShotResult(position, collision, excludeTeam=self._ShotResultIndicatorPlugin__playerTeam)
-        if result in self._ShotResultIndicatorPlugin__colors and collision and collision.isVehicle():
-            self.indicator.setInfo(collision[2], collision[1], result)
-            self.indicator.setVisible(True)
-        else:
-            self.indicator.setVisible(False)
+        if self.indicator:
+            if result in self._ShotResultIndicatorPlugin__colors and collision and collision.isVehicle():
+                self.indicator.setInfo(collision.armor, collision.hitAngleCos, result)
+                self.indicator.setVisible(True)
+            else:
+                self.indicator.setInfo(None, None, None)
+                self.indicator.setVisible(True)
 
     def __onGunMarkerStateChanged(self, markerType, position, _, collision):
         if self._ShotResultIndicatorPlugin__isEnabled:
@@ -123,3 +133,46 @@ def _createPlugins():
 
 _createPlugins_orig = plugins.createPlugins
 plugins.createPlugins = _createPlugins
+
+_SHOT_RESULT = aih_constants.SHOT_RESULT
+
+def getShotResult(hitPoint, collision, excludeTeam = 0):
+    """ Gets shot result by present state of gun marker.
+    :param hitPoint: Vector3 containing shot position.
+    :param collision: instance of EntityCollisionData.
+    :param excludeTeam: integer containing number of team that is excluded from result.
+    :return: one of SHOT_RESULT.*.
+    """
+    if collision is None:
+        return _SHOT_RESULT.UNDEFINED
+    else:
+        entity = collision.entity
+        if entity.health <= 0 or entity.publicInfo['team'] == excludeTeam:
+            return _SHOT_RESULT.UNDEFINED
+        player = BigWorld.player()
+        if player is None:
+            return _SHOT_RESULT.UNDEFINED
+        vDesc = player.getVehicleDescriptor()
+        ppDesc = vDesc.shot['piercingPower']
+        maxDist = vDesc.shot['maxDistance']
+        dist = (hitPoint - player.getOwnVehiclePosition()).length
+        if dist <= 100.0:
+            piercingPower = ppDesc[0]
+        elif maxDist > dist:
+            p100, p500 = ppDesc
+            piercingPower = p100 + (p500 - p100) * (dist - 100.0) / 400.0
+            if piercingPower < 0.0:
+                piercingPower = 0.0
+        else:
+            piercingPower = 0.0
+        piercingPercent = 1000.0
+        if piercingPower > 0.0:
+            armor = collision.armor / collision.hitAngleCos
+            piercingPercent = 100.0 + (armor - piercingPower) / piercingPower * 100.0
+        if piercingPercent >= 150:
+            result = _SHOT_RESULT.NOT_PIERCED
+        elif 90 < piercingPercent < 150:
+            result = _SHOT_RESULT.LITTLE_PIERCED
+        else:
+            result = _SHOT_RESULT.GREAT_PIERCED
+        return result
